97db3058119755520fd2160743caa2428accb4ef
haider-zaidi72 <suhz1100@gmail.com>
2025-11-19 09:06:44 +0500

    add

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  base: "/ink-against-the-wind/",   // <-- add this
+  plugins: [react()],
+});
