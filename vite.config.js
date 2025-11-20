8e0cd9e0a94008a4896d7749774db188c6b55303
haider-zaidi72 <suhz1100@gmail.com>
2025-11-20 08:20:29 +0500

    addVercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ink-against-the-wind/", // important for GitHub Pages
   plugins: [react()],
+  base: "/", // VERY IMPORTANT for Vercel
 });
