f18ba21d4de8b577cbba2ecd29e3f6d9a3825f33
haider-zaidi72 <digimind1100@gmail.com>
2026-03-29 17:17:22 +0500

    update navbar and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Novel from "./components/pages/Novel";
 import Blog from "./components/pages/Blog";
 import Events from "./components/pages/Events";
 import Travel from "./components/pages/Travel";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Gallery from "./components/pages/Gallery";
+import TranslatedWork from "./components/pages/TranslatedWork";
 
 
 function App() {
@@ -26,6 +30,10 @@ function App() {
           <Route path="/novel" element={<Novel />} />
           <Route path="/events" element={<Events />} />
           <Route path="/travel" element={<Travel />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/translated-work" element={<TranslatedWork />} />
 
 
         </Route>
