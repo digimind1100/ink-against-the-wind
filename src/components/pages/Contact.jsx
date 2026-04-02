b106b62ddbd46501411c7a8739bff9795804546d
haider-zaidi72 <digimind1100@gmail.com>
2026-04-02 20:27:45 +0500

    update padding

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -41,7 +41,7 @@ export default function Contact() {
       </div>
 
       {/* 📩 Form */}
-      <div className="min-h-screen bg-black text-white pt-24 md:pt-28">
+      <div className="flex justify-center px-6 pb-20 pt-24">
         <form
           ref={form}
           onSubmit={sendEmail}
